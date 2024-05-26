259f8a13758f6d20844345ee1d8febbd6af0824c
unknown <victornaokisatisato.com>
2024-05-26 17:42:29 -0300

    Trying to implement Gemini 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/firebase_options.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image';
 import 'app.dart';
 
 
 
 Future<void> main() async {
-  Gemini.init(apiKey: GEMINI_API_KEY);
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/paginas/const.dart
+++ lib/paginas/const.dart
@@ -1,1 +1,1 @@
-const String GEMINI_API_KEY = "AIzaSyARlwWH_xoKl2SanGiiJWGBL9MMvb6q2ik"
+const String GEMINI_API_KEY = "AIzaSyARlwWH_xoKl2SanGiiJWGBL9MMvb6q2ik";
